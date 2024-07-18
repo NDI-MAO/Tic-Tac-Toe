b526654537b58889b0c291a81b32887e00f246d6
Rugwiza <drugwiza@gmail.com>
2024-07-18 11:04:17 +0200

    ajusting XO color for a better UI and testing click option with js

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const gridFace = document.querySelectorAll(".gameGrid");
 //gridFace[2].innerText = "○";
 //gridFace[5].innerText="×";
 
-const gameboard = [gridFace[0], gridFace[1], gridFace[2], gridFace[3], gridFace[4], gridFace[5], gridFace[6], gridFace[7], gridFace[8]];
-
+const gameboard = Array.from(gridFace);
 
 // Function to handle the click event
 function handleClick(event) {
   const element = event.target;
-  element.textContent="○";
+  element.textContent= "○";
 }
 
 // Attach click event handler to each grid item
