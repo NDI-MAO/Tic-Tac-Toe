22115d5ad41df54633ce4972b0844685cf90fb41
Rugwiza <drugwiza@gmail.com>
2024-07-25 09:38:24 +0200

    adding console log to troubleshoot

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let playerArray = [];
 let botArray =[];
 
 // Function to handle the player's click event
-function handleClick(event) {
+function playerChoice(event) {
   const element = event.target;
   element.textContent= "○";
   newGameboard();
@@ -13,7 +13,7 @@ function handleClick(event) {
 
 // Attach click event handler to each grid item
 gridFace.forEach(item => {
-  item.addEventListener('click', handleClick);
+  item.addEventListener('click', playerChoice);
 });
 
 //Function to update gameboard
@@ -29,18 +29,6 @@ function newGameboard(){
         }
         return accumulator;
     },[]);
-  console.log('Updated gameboard:', gameboard);
   console.log('Player array:', playerArray);
-  //console.log('bot array:', botArray);
+  console.log(gameboard);
 }
-
-
-//trying function to pick a random gridface
-function botChoice(){
-    return gameboard[Math.floor(Math.random()*gameboard.length)];
-}
-
-
-
-console.log(gameboard);  // Output: [1, 3, 5]
-console.log(playerArray);  // Output: [2, 4]
